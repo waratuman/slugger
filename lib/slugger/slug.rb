def37e63b1d7c3585afae2ee3b41847cad677286
James R. Bracy <waratuman@gmail.com>
2015-03-20 14:12:02 -0700

    removing the default scope

--- lib/slugger/slug.rb
+++ lib/slugger/slug.rb
@@ -2,8 +2,6 @@ module Slugger
   class Slug < ActiveRecord::Base
     table_name = 'slugs'
 
-    default_scope -> { order(:created_at => :desc) }
-
     belongs_to :model, polymorphic: true
 
     def to_param
